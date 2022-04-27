96adcf201277a1ab4406152625d45c9264ec0805
wu-chang-kang <1147552381@qq.com>
2022-04-27 23:28:39 +0800

    Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore } from 'vuex'
 
+// 创建vuex仓库并导出
 export default createStore({
   state: {
-  },
-  getters: {
+    // 数据
   },
   mutations: {
+    // 改数据函数
   },
   actions: {
+    // 请求数据函数
   },
   modules: {
+    // 分模块
+  },
+  getters: {
+    // vuex的计算属性
   }
 })
